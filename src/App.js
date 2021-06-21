2665b5f30ba259be1f02321659e175eccfaa0b7b
Yakov Falik <falikyakov@gmail.com>
2021-06-22 01:02:58 +0300

    ok1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
